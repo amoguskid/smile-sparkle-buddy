4923b2481ae87787c97b58fb7c01d9125d842a0a
amoguskid <114032655+amoguskid@users.noreply.github.com>
2026-07-18 15:06:59 -0700

    Use existing App Store bundle ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.d0873f1f472144159d9632b3342ea708',
+  appId: 'com.despia.brushingapp',
   appName: 'smile-sparkle-buddy',
   webDir: 'dist'
 };
